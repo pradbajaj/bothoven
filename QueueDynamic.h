12b49874b6a30e14cf5efd372b080f1b54198f85
justanotherdevel <shashwatyashaswi@gmail.com>
2016-12-07 18:58:51 +0530

    Updated DynamicQueue.h to better suit BFS

--- QueueDynamic.h
+++ QueueDynamic.h
@@ -44,10 +44,9 @@ int DeQueue(struct Queue *Q)
 {
 	if(Q->head==NULL && Q->tail==NULL)
 	{
-		cout<<"The Queue is empty .";
 		return -1;
 	}
-	struct Node *temp= Q->head;
+	struct Node *temp = Q->head;
 	int data = temp->data;
 	Q->head=Q->head->next;
 	if(Q->head==NULL)
@@ -55,40 +54,14 @@ int DeQueue(struct Queue *Q)
 	delete temp;
 	return data;
 }
-void Print(struct Queue *Q)
-{
-	if(Q->head==NULL && Q->tail==NULL)
-	{
-		cout<<"The Queue is empty.\n";
-		return;
-	}
-	struct Node *temp = Q->head;
-	for(temp=Q->head; temp!=Q->tail->next ; temp=temp->next)
-		cout<<temp->data<<" ";
-	cout<<"\n";
-}
+
 void EmptyQueue(struct Queue *Q)
 {
 	while(Q->head!=NULL)
 		DeQueue(Q);
-	cout<<"\n The Queue is now empty.\n\n";
-	system("pause");
 }
+
 bool IsEmpty(struct Queue *Q)
 {
 	return (Q->head==NULL) ? true : false ;
 }
-void FreeQueue(struct Queue *Q)
-{
-	if(IsEmpty(Q)==false)
-	{
-		cout<<"\n The queue is not empty. First empty the queue.\n\n";
-		system("pause");
-		choice=-1;
-		return;
-	}
-	delete Q;
-	cout<<"\n The memory is now cleared.\n\n";	
-	system("pause");
-}
-
