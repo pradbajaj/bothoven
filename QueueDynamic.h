7a34fe0390491248b63eaf476232eeef3391768f
justanotherdevel <shashwatyashaswi@gmail.com>
2016-12-19 12:51:59 +0530

    Completed dStat (at least I think so)

--- QueueDynamic.h
+++ QueueDynamic.h
@@ -1,37 +1,30 @@
-#include<iostream>
-using namespace std;
+#include <cstdlib>
 
 int choice;
-struct Node
-{
+struct Node {
 	int data;
 	struct Node* next;
 };
-struct Queue
-{
+struct Queue {
 	struct Node *head, *tail;  
 };
 
-struct Node * NewNode()
-{
-	struct Node *temp = new struct Node;
+struct Node * NewNode() {
+	struct Node *temp = (struct Node*) malloc (sizeof(struct Node));
 	temp->next=NULL;
 	return temp;
 }
-struct Queue * NewQueue()
-{
-	struct Queue *Q = new struct Queue;
+struct Queue * NewQueue() {
+	struct Queue *Q = (struct Queue*) malloc (sizeof(struct Queue));
 	Q->head=NULL;
 	Q->tail=NULL;
 	return Q;
 }
 
-void EnQueue(struct Queue *Q, int data)
-{
+void EnQueue(struct Queue *Q, int data) {
 	struct Node * temp= NewNode();
 	temp->data=data;
-	if(Q->head==NULL && Q->tail==NULL)
-	{
+	if(Q->head==NULL && Q->tail==NULL) {
 		Q->head=temp;
 		Q->tail=temp;
 		return;
@@ -40,10 +33,8 @@ void EnQueue(struct Queue *Q, int data)
 	Q->tail=temp;
 }
 
-int DeQueue(struct Queue *Q)
-{
-	if(Q->head==NULL && Q->tail==NULL)
-	{
+int DeQueue(struct Queue *Q) {
+	if(Q->head==NULL && Q->tail==NULL) {
 		return -1;
 	}
 	struct Node *temp = Q->head;
@@ -51,17 +42,15 @@ int DeQueue(struct Queue *Q)
 	Q->head=Q->head->next;
 	if(Q->head==NULL)
 		Q->tail=NULL;
-	delete temp;
+	free (temp);	
 	return data;
 }
 
-void EmptyQueue(struct Queue *Q)
-{
+void EmptyQueue(struct Queue *Q) {
 	while(Q->head!=NULL)
 		DeQueue(Q);
 }
 
-bool IsEmpty(struct Queue *Q)
-{
+bool IsEmpty(struct Queue *Q){
 	return (Q->head==NULL) ? true : false ;
 }
