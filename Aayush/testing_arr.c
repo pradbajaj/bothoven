32410c70a6aa63934aff8c8cb4a3a89e7d4aa428
aayushjain0829 <aayushjain0829@gmail.com>
2017-02-26 10:39:04 +0530

    Added a folder Aayush with Serial.py file explaining how to send data to master bot after finding nodes. Added another file testing_arr.c depicting how master bot will recieve the nodes array from the python file

---
+++ Aayush/testing_arr.c
@@ -0,0 +1,436 @@
+/********************************************************************************
+ Written by: Vinod Desai, NEX Robotics Pvt. Ltd.
+ Edited by: Sachitanand Malewar, NEX Robotics Pvt. Ltd.
+ AVR Studio Version 4.17, Build 666
+
+ Date: 26th December 2010
+ 
+ Application example: Robot control over serial port via USB-RS232 converter 
+ 					  (located on the ATMEGA260 microcontroller adaptor board)
+
+ Concepts covered:  serial communication
+ 
+ Serial Port used: UART2
+
+ There are two components to the motion control:
+ 1. Direction control using pins PORTA0 to PORTA3
+ 2. Velocity control by PWM on pins PL3 and PL4 using OC5A and OC5B.
+
+ In this experiment for the simplicity PL3 and PL4 are kept at logic 1.
+ 
+ Pins for PWM are kept at logic 1.
+  
+ Connection Details:  	
+ 						
+  Motion control:		L-1---->PA0;		L-2---->PA1;
+   						R-1---->PA2;		R-2---->PA3;
+   						PL3 (OC5A) ----> Logic 1; 	PL4 (OC5B) ----> Logic 1; 
+
+
+  Serial Communication:	PORTD 2 --> RXD1 UART1 receive for RS232 serial communication
+						PORTD 3 --> TXD1 UART1 transmit for RS232 serial communication
+
+						PORTH 0 --> RXD2 UART 2 receive for USB - RS232 communication
+						PORTH 1 --> TXD2 UART 2 transmit for USB - RS232 communication
+
+						PORTE 0 --> RXD0 UART0 receive for ZigBee wireless communication
+						PORTE 1 --> TXD0 UART0 transmit for ZigBee wireless communication
+
+						PORTJ 0 --> RXD3 UART3 receive available on microcontroller expansion socket
+						PORTJ 1 --> TXD3 UART3 transmit available on microcontroller expansion socket
+
+Serial communication baud rate: 9600bps
+To control robot use number pad of the keyboard which is located on the right hand side of the keyboard.
+Make sure that NUM lock is on.
+
+Commands:
+			Keyboard Key   ASCII value	Action
+				8				0x38	Forward
+				2				0x32	Backward
+				4				0x34	Left
+				6				0x36	Right
+				5				0x35	Stop
+				7				0x37	Buzzer on
+				9				0x39	Buzzer off
+
+ Note: 
+ 
+ 1. Make sure that in the configuration options following settings are 
+ 	done for proper operation of the code
+
+ 	Microcontroller: atmega2560
+ 	Frequency: 14745600
+ 	Optimization: -O0  (For more information read section: Selecting proper optimization 
+ 					options below figure 2.22 in the Software Manual)
+
+ 2. Difference between the codes for RS232 serial, USB and wireless communication is only in the serial port number.
+ 	Rest of the things are the same. 
+
+ 3. For USB communication check the Jumper 1 position on the ATMEGA2560 microcontroller adaptor board
+ 
+ 4. Auxiliary power can supply current up to 1 Ampere while Battery can supply current up to 
+ 	2 Ampere. When both motors of the robot changes direction suddenly without stopping, 
+	it produces large current surge. When robot is powered by Auxiliary power which can supply
+	only 1 Ampere of current, sudden direction change in both the motors will cause current 
+	surge which can reset the microcontroller because of sudden fall in voltage. 
+	It is a good practice to stop the motors for at least 0.5seconds before changing 
+	the direction. This will also increase the useable time of the fully charged battery.
+	the life of the motor.
+
+*********************************************************************************/
+
+/********************************************************************************
+
+   Copyright (c) 2010, NEX Robotics Pvt. Ltd.                       -*- c -*-
+   All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are met:
+
+   * Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+
+   * Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in
+     the documentation and/or other materials provided with the
+     distribution.
+
+   * Neither the name of the copyright holders nor the names of
+     contributors may be used to endorse or promote products derived
+     from this software without specific prior written permission.
+
+   * Source code can be used for academic purpose. 
+	 For commercial use permission form the author needs to be taken.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+  POSSIBILITY OF SUCH DAMAGE. 
+
+  Software released under Creative Commence cc by-nc-sa licence.
+  For legal information refer to: 
+  http://creativecommons.org/licenses/by-nc-sa/3.0/legalcode
+
+********************************************************************************/
+
+#define F_CPU 14745600
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#define RS 0
+#define RW 1
+#define EN 2
+#define lcd_port PORTC
+
+#define sbit(reg,bit)	reg |= (1<<bit)
+#define cbit(reg,bit)	reg &= ~(1<<bit)
+
+unsigned int temp;
+unsigned int unit;
+unsigned int tens;
+unsigned int hundred;
+unsigned int thousand;
+unsigned int million;
+
+int i;
+
+unsigned char data; //to store received data from UDR1
+
+
+//Function to configure LCD port
+void lcd_port_config (void)
+{
+	DDRC = DDRC | 0xF7; //all the LCD pin's direction set as output
+	PORTC = PORTC & 0x80; // all the LCD pins are set to logic 0 except PORTC 7
+}
+
+//Function to initialize ports
+void port_init()
+{
+	lcd_port_config();
+}
+
+/*****Function to Reset LCD*****/
+void lcd_set_4bit()
+{
+	_delay_ms(1);
+
+	cbit(lcd_port,RS);				//RS=0 --- Command Input
+	cbit(lcd_port,RW);				//RW=0 --- Writing to LCD
+	lcd_port = 0x30;				//Sending 3
+	sbit(lcd_port,EN);				//Set Enable Pin
+	_delay_ms(5);					//Delay
+	cbit(lcd_port,EN);				//Clear Enable Pin
+
+	_delay_ms(1);
+
+	cbit(lcd_port,RS);				//RS=0 --- Command Input
+	cbit(lcd_port,RW);				//RW=0 --- Writing to LCD
+	lcd_port = 0x30;				//Sending 3
+	sbit(lcd_port,EN);				//Set Enable Pin
+	_delay_ms(5);					//Delay
+	cbit(lcd_port,EN);				//Clear Enable Pin
+
+	_delay_ms(1);
+
+	cbit(lcd_port,RS);				//RS=0 --- Command Input
+	cbit(lcd_port,RW);				//RW=0 --- Writing to LCD
+	lcd_port = 0x30;				//Sending 3
+	sbit(lcd_port,EN);				//Set Enable Pin
+	_delay_ms(5);					//Delay
+	cbit(lcd_port,EN);				//Clear Enable Pin
+
+	_delay_ms(1);
+
+	cbit(lcd_port,RS);				//RS=0 --- Command Input
+	cbit(lcd_port,RW);				//RW=0 --- Writing to LCD
+	lcd_port = 0x20;				//Sending 2 to initialise LCD 4-bit mode
+	sbit(lcd_port,EN);				//Set Enable Pin
+	_delay_ms(5);					//Delay
+	cbit(lcd_port,EN);				//Clear Enable Pin
+
+	
+}
+
+/*****Function to Initialize LCD*****/
+void lcd_init()
+{
+	_delay_ms(1);
+
+	lcd_wr_command(0x28);			//LCD 4-bit mode and 2 lines.
+	lcd_wr_command(0x01);
+	lcd_wr_command(0x06);
+	lcd_wr_command(0x0E);
+	lcd_wr_command(0x80);
+	
+}
+void lcd_home()
+{
+	lcd_wr_command(0x80);
+}
+
+/*****Function to Write Command on LCD*****/
+void lcd_wr_command(unsigned char cmd)
+{
+	unsigned char temp;
+	temp = cmd;
+	temp = temp & 0xF0;
+	lcd_port &= 0x0F;
+	lcd_port |= temp;
+	cbit(lcd_port,RS);
+	cbit(lcd_port,RW);
+	sbit(lcd_port,EN);
+	_delay_ms(5);
+	cbit(lcd_port,EN);
+	
+	cmd = cmd & 0x0F;
+	cmd = cmd<<4;
+	lcd_port &= 0x0F;
+	lcd_port |= cmd;
+	cbit(lcd_port,RS);
+	cbit(lcd_port,RW);
+	sbit(lcd_port,EN);
+	_delay_ms(5);
+	cbit(lcd_port,EN);
+}
+
+/*****Function to Write Data on LCD*****/
+void lcd_wr_char(char letter)
+{
+	char temp;
+	temp = letter;
+	temp = (temp & 0xF0);
+	lcd_port &= 0x0F;
+	lcd_port |= temp;
+	sbit(lcd_port,RS);
+	cbit(lcd_port,RW);
+	sbit(lcd_port,EN);
+	_delay_ms(5);
+	cbit(lcd_port,EN);
+
+	letter = letter & 0x0F;
+	letter = letter<<4;
+	lcd_port &= 0x0F;
+	lcd_port |= letter;
+	sbit(lcd_port,RS);
+	cbit(lcd_port,RW);
+	sbit(lcd_port,EN);
+	_delay_ms(5);
+	cbit(lcd_port,EN);
+}
+
+void lcd_string(char *str)
+{
+	while(*str != '\0')
+	{
+		lcd_wr_char(*str);
+		str++;
+	}
+}
+
+void lcd_cursor (char row, char column)
+{
+	switch (row) {
+		case 1: lcd_wr_command (0x80 + column - 1); break;
+		case 2: lcd_wr_command (0xc0 + column - 1); break;
+		case 3: lcd_wr_command (0x94 + column - 1); break;
+		case 4: lcd_wr_command (0xd4 + column - 1); break;
+		default: break;
+	}
+}
+/***** Function To Print Any input value upto the desired digit on LCD *****/
+void lcd_print (char row, char coloumn, unsigned int value, int digits)
+{
+	unsigned char flag=0;
+	if(row==0||coloumn==0)
+	{
+		lcd_home();
+	}
+	else
+	{
+		lcd_cursor(row,coloumn);
+	}
+	if(digits==5 || flag==1)
+	{
+		million=value/10000+48;
+		lcd_wr_char(million);
+		flag=1;
+	}
+	if(digits==4 || flag==1)
+	{
+		temp = value/1000;
+		thousand = temp%10 + 48;
+		lcd_wr_char(thousand);
+		flag=1;
+	}
+	if(digits==3 || flag==1)
+	{
+		temp = value/100;
+		hundred = temp%10 + 48;
+		lcd_wr_char(hundred);
+		flag=1;
+	}
+	if(digits==2 || flag==1)
+	{
+		temp = value/10;
+		tens = temp%10 + 48;
+		lcd_wr_char(tens);
+		flag=1;
+	}
+	if(digits==1 || flag==1)
+	{
+		unit = value%10 + 48;
+		lcd_wr_char(unit);
+	}
+	if(digits>5)
+	{
+		lcd_wr_char('E');
+	}
+	
+}
+
+
+//Function To Initialize UART2
+// desired baud rate:9600
+// actual baud rate:9600 (error 0.0%)
+// char size: 8 bit
+// parity: Disabled
+void uart2_init(void)
+{
+ UCSR2B = 0x00; //disable while setting baud rate
+ UCSR2A = 0x00;
+ UCSR2C = 0x06;
+ UBRR2L = 0x5F; //set baud rate lo
+ UBRR2H = 0x00; //set baud rate hi
+ UCSR2B = 0x98;
+}
+
+signed int count = -1;
+signed int size = 0;
+signed int *arr;
+
+void print() {
+	int k = 1;
+	int j = 1;
+	for (int i = 0; i < size; ++i)
+	{
+		//lcd_wr_char(arr[i]);
+		if (j == 17)
+		{
+			j = 1;
+			k = 2;
+		}
+
+		lcd_print(k,j,arr[i],2);
+		j += 2;
+	}
+}
+
+SIGNAL(SIG_USART2_RECV) 		// ISR for receive complete interrupt
+{
+	data = UDR2; 				//making copy of data from UDR2 in 'data' variable 
+
+	UDR2 = data; 				//echo data back to PC
+	if (count == -1)
+	{
+		size = (signed int) data;
+		size -= 48;
+		count++;
+		arr = (signed int*) malloc(size*sizeof(signed int));
+		//lcd_wr_char(data);
+		//lcd_print(1,1,size,2);
+
+	} 
+	
+	else if (count < size) {
+		arr[count] = (signed int) data;
+		arr[count] -= 48;
+		count++;
+		//lcd_wr_char(arr[count-1]);
+		//lcd_wr_char((int)count);
+		//lcd_print(1,count+1,arr[count-1],1);
+	}
+	else {
+		//print();
+		/*for (int i = 0; i < size; ++i)
+		{
+			lcd_wr_char(arr[i]);
+		}*/
+		//lcd_wr_char(data);
+		//lcd_cursor(2,1);
+		//lcd_string("All recieved");
+		//lcd_cursor(1,1);
+		print();
+	}
+
+	//lcd_wr_char(data);
+		
+}
+
+
+//Function To Initialize all The Devices
+void init_devices()
+{
+ cli(); //Clears the global interrupts
+ port_init();  //Initializes all the ports
+ uart2_init(); //Initailize UART1 for serial communiaction
+ sei();   //Enables the global interrupts
+}
+
+//Main Function
+int main(void)
+{
+	init_devices();
+	lcd_set_4bit();
+	lcd_init();
+		while(1);
+}
+
